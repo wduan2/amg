3e55715bf991058e24374175d416bf01d883fc99
Wenyu Duan <duanwenyu1988@gmail.com>
2016-02-24 00:06:40 -0600

    Use awesome_print to print result.

--- app/main.rb
+++ app/main.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/ruby
 
+require 'ap'
 require 'optparse'
 require_relative 'utils/db_util'
 
@@ -8,21 +9,16 @@ class Main
     begin
       OptionParser.new do |opts|
         opts.on('-l', '--list', 'list all') do
-          result = DbUtil.list_all
-          result.each do |row|
-            puts(row)
-          end
+          result = DbUtil.list_all.collect { |row| row }
+          ap result
         end
 
         opts.on('-f', '--find [label]', 'find account') do |label|
-          result = DbUtil.find(label)
-          result.each do |row|
-            puts(row)
-          end
+          result = DbUtil.find(label) { |row| row }
+          ap result
         end
 
         opts.on('-a', '--add [label],[username],[password]', Array, 'add new account') do |acct_info|
-          puts(acct_info)
           DbUtil.add_new(acct_info[0], acct_info[1], acct_info[2])
         end
       end.parse!
